49380c284d805cd423e15b1117ce03f53ae94bf7
mohamedAlaaElamer <elamermohamed440@gmail.com>
2022-10-26 10:42:22 +0200

    add views and urls

--- job/views.py
+++ job/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
+from .models import Job
 # Create your views here.
 
 def job_list(request):
-    pass
+    job_list  = Job.objects.all()
+    context = {'jobs':job_list}
+    return render(request,'job/job_list.html',context)
 
 def job_detail(requset, id):
     pass
